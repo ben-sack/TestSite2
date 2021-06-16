579905db8ef83e76a8ba725cd9614c4d7607e4be
ben-sack <mr.benjaminsack@gmail.com>
2021-06-16 08:26:20 -0600

    fixed menu + swup functionality

--- script.js
+++ script.js
@@ -1,43 +1,5 @@
 //Swup (page transitions)
-
-//Welcome Animation
-const spans = document.querySelectorAll('.welcome-container span')
-
-spans.forEach((span, idx) => {
-	span.addEventListener('click', (e) => {
-		e.target.classList.add('active');
-	});
-	span.addEventListener('animationend', (e) => {
-		e.target.classList.remove('active');
-	});
-
-	// Initial animation
-	setTimeout(() => {
-		span.classList.add('active');
-	}, 750 * (idx + 1))
-});
-
-//Show menu items function
-var l1 = document.getElementById('li1')
-var l2 = document.getElementById('li2')
-var l3 = document.getElementById('li3')
-
-function showMenu(){
-
-	console.log(l1.style.visibility)
-
-	if (l3.style.visibility == 'hidden'){
-		setTimeout(function(){l1.style.visibility = "visible"}, 500)
-		setTimeout(function(){l2.style.visibility = "visible"}, 300)
-		setTimeout(function(){l3.style.visibility = "visible"}, 700)
-
-	} else{
-		setTimeout(function(){l1.style.visibility = "hidden"}, 300)
-		setTimeout(function(){l2.style.visibility = "hidden"}, 700)
-		setTimeout(function(){l3.style.visibility = "hidden"}, 500)
-	}
-}
-
+const swup = new Swup();
 
 // Hide initial welcome animation
 var welcome = document.getElementById('hide-me')
@@ -50,10 +12,34 @@ function hideWelcome(){
 		setTimeout(function(){welcome.style.opacity = 0.2},400)
 		setTimeout(function(){welcome.style.opacity = 0},500)
 		setTimeout(function(){welcome.style.visibility = 'hidden'},500)
-
 	}
 }
 setTimeout(hideWelcome,7000)
+
+//Welcome Animation
+const spans = document.querySelectorAll('.welcome-container span')
+function animateLetter() {
+	spans.forEach((span, idx) => {
+		span.addEventListener('click', (e) => {
+			e.target.classList.add('active');
+		});
+		span.addEventListener('animationend', (e) => {
+			e.target.classList.remove('active');
+		});
+
+		// Initial animation
+		setTimeout(() => {
+			span.classList.add('active');
+		}, 750 * (idx + 1))
+	});
+}
+//Show menu items function
+var l1 = document.getElementById('li1')
+var l2 = document.getElementById('li2')
+var l3 = document.getElementById('li3')
+var menu = document.getElementById('menu-wrapper')
+
+
 
 
 // Shows main content after welcome message
@@ -64,22 +50,26 @@ var delay_text = document.getElementById('delay-text')
 var delay_text2 = document.getElementById('delay-text2')
 
 //hide content function with delay + opacity to cause blur effect
-function hide(){
-	if (content1.style.visibility == 'visible'){
-		setTimeout(function(){content1.style.opacity = 0.8},100)
-		setTimeout(function(){content1.style.opacity = 0.6},200)
-		setTimeout(function(){content1.style.opacity = 0.4},300)
-		setTimeout(function(){content1.style.opacity = 0.2},400)
-		setTimeout(function(){content1.style.opacity = 0},500)
+function hide() {
+	if (content1 !== null) {
+		if (content1.style.visibility == 'visible') {
+			setTimeout(function () { content1.style.opacity = 0.8 }, 100)
+			setTimeout(function () { content1.style.opacity = 0.6 }, 200)
+			setTimeout(function () { content1.style.opacity = 0.4 }, 300)
+			setTimeout(function () { content1.style.opacity = 0.2 }, 400)
+			setTimeout(function () { content1.style.opacity = 0 }, 500)
+		}
 	}
 }
 function hide2(){
-	if (content2.style.visibility == 'visible'){
-		setTimeout(function(){content2.style.opacity = 0.8},100)
-		setTimeout(function(){content2.style.opacity = 0.6},200)
-		setTimeout(function(){content2.style.opacity = 0.4},300)
-		setTimeout(function(){content2.style.opacity = 0.2},400)
-		setTimeout(function(){content2.style.opacity = 0},500)
+	if (content1 !== null) {
+		if (content2.style.visibility == 'visible') {
+			setTimeout(function () { content2.style.opacity = 0.8 }, 100)
+			setTimeout(function () { content2.style.opacity = 0.6 }, 200)
+			setTimeout(function () { content2.style.opacity = 0.4 }, 300)
+			setTimeout(function () { content2.style.opacity = 0.2 }, 400)
+			setTimeout(function () { content2.style.opacity = 0 }, 500)
+		}
 	}
 }
 // function hide3(){
@@ -103,43 +93,85 @@ setTimeout(hide2, 12300)
 //show main_content
 setTimeout(function(){main_content.style.visibility = 'visible'}, 13000)
 
+//Gallery
+var option = document.querySelector(".option")
 
-//theme options
-let theme = localStorage.getItem('theme')
-
-if(theme == null){
-	setTheme('light')
-}else{
-	setTheme(theme)
+//wrapped in javascript funtion
+function gallery() {
+	$(".option").click(function () {
+		$(".option").removeClass("active");
+		$(this).addClass("active");
+	});
 }
+setTimeout(gallery,1)
 
-let themeDots = document.getElementsByClassName('theme-dot')
 
 
-for (var i=0; themeDots.length > i; i++){
-	themeDots[i].addEventListener('click', function(){
-		let mode = this.dataset.mode
-		console.log('Option clicked:', mode)
-		setTheme(mode)
-	})
-}
+function init() {
 
-function setTheme(mode){
-	if(mode == 'light'){
-		document.getElementById('theme-style').href = 'default.css'
+	//theme options
+	let theme = localStorage.getItem('theme')
+	if(theme == null){
+		setTheme('light')
+	}else{
+		setTheme(theme)
 	}
 
-	if(mode == 'blue'){
-		document.getElementById('theme-style').href = 'blue.css'
+	let themeDots = document.getElementsByClassName('theme-dot')
+	for (var i=0; themeDots.length > i; i++){
+		themeDots[i].addEventListener('click', function(){
+			let mode = this.dataset.mode
+			console.log('Option clicked:', mode)
+			setTheme(mode)
+		})
 	}
 
-	if(mode == 'green'){
-		document.getElementById('theme-style').href = 'green.css'
+	function setTheme(mode){
+		if(mode == 'light'){
+			document.getElementById('theme-style').href = 'default.css'
+		}
+
+		if(mode == 'blue'){
+			document.getElementById('theme-style').href = 'blue.css'
+		}
+
+		if(mode == 'green'){
+			document.getElementById('theme-style').href = 'green.css'
+		}
+
+		if(mode == 'purple'){
+			document.getElementById('theme-style').href = 'purple.css'
+		}
+
+		localStorage.setItem('theme', mode)
 	}
 
-	if(mode == 'purple'){
-		document.getElementById('theme-style').href = 'purple.css'
-	}
+	setTimeout(gallery,1)
+};
 
-	localStorage.setItem('theme', mode)
+init();
+document.addEventListener('swup:contentReplaced', init);
+
+// toggles menu item visibility by cheacking length of class list (0 or >0)
+function toggleMenu(){
+	const lis = document.querySelectorAll('#nav-wrapper li')
+
+	function showMenu(){
+		lis.forEach((li, idx) => {
+			li.classList.add('appear')
+		});
+	};
+
+	function hideMenu(){
+		lis.forEach((li, idx) => {
+			li.classList.remove('appear')	
+		});
+	};
+
+	var a = document.getElementsByClassName('appear')
+	if (a.length == 0){
+		showMenu()
+	} else{
+		hideMenu()
+	};
 }
